4abb4c4a5150a43c3ac793fe9f89c85383a40f7c
wonzerL <58498195+wonzerL@users.noreply.github.com>
2022-03-31 13:14:27 +0800

    two panel

--- src/main/MainFrame.java
+++ src/main/MainFrame.java
@@ -2,8 +2,12 @@ package main;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.awt.*;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import com.alibaba.fastjson.*;
 
@@ -17,10 +21,13 @@ public class MainFrame extends JFrame implements ActionListener {
     private MainFrame mainFrame;
     private BasePanel panels[] = new BasePanel[Panels.values().length];
     private JSONObject nowInfo;
+    private JPanelWithBackground bgPanel;
 
     public void goPanel(Panels nowPanel, Panels targetPanel) {
         // TODO: 如果有可能的话做成滑动的切换效果？
         // set now panel invisible
+
+        // this.getContentPane().remove(bgPanel);
         panels[nowPanel.ordinal()].setEnabled(false);
         panels[nowPanel.ordinal()].setVisible(false);
 
@@ -63,11 +70,40 @@ public class MainFrame extends JFrame implements ActionListener {
         // firstPanel.setVisible(true);
         this.setTitle("G14's Kiosk Program");
         this.setSize(1024, 768);
+        try {
+            this.bgPanel = new JPanelWithBackground(
+                    "resources/img/bg.jpeg");
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+        // TODO：背景图还有问题，解除注释可以看到图，但第二页会无法显示
+        // this.getContentPane().add(this.bgPanel);
+
         this.setLocationRelativeTo(null);
         this.setResizable(false);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
 
+    }
+
+    public class JPanelWithBackground extends JPanel {
+
+        private Image backgroundImage;
+
+        // Some code to initialize the background image.
+        // Here, we use the constructor to load the image. This
+        // can vary depending on the use case of the panel.
+        public JPanelWithBackground(String fileName) throws IOException {
+            backgroundImage = ImageIO.read(MainFrame.class.getClassLoader().getResource(fileName));
+        }
+
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            // Draw the background image.
+            g.drawImage(backgroundImage.getScaledInstance(1024, 768, Image.SCALE_SMOOTH), 0, 0, this);
+        }
     }
 
     public void actionPerformed(ActionEvent e) {
