fadacbab07127d3f6be62daee1f9f404abf9cbbc
huliang <huliang.2022@bytedance.com>
2022-04-10 17:48:06 +0800

    iter2 数据模块

---
+++ src/main/utils/DataService.java
@@ -0,0 +1,80 @@
+package main.utils;
+
+import com.alibaba.fastjson.*;
+
+public class DataService {
+    private JSONArray allPerson;
+    private JSONArray allFlight;
+
+    public DataService() {
+        refreshData();
+    }
+
+    public JSONArray getAllPerson() {
+        return allPerson;
+    }
+
+    public JSONArray getAllFlight() {
+        return allFlight;
+    }
+
+    public JSONObject getFlightById(String id) {
+        for (int i = 0; i < this.allFlight.size(); i++) {
+            JSONObject flight = this.allFlight.getJSONObject(i);
+            if (flight.getString("flightNo").equals(id)) {
+                return flight;
+            }
+        }
+        return null;
+    }
+
+    public JSONObject getPersonById(String id) {
+        for (int i = 0; i < this.allPerson.size(); i++) {
+            JSONObject person = this.allPerson.getJSONObject(i);
+            if (person.getJSONObject("baseInfo").getString("id").equals(id)) {
+                return person;
+            }
+        }
+        return null;
+    }
+
+    public JSONObject getPersonByBookingNo(String no) {
+        for (int i = 0; i < this.allPerson.size(); i++) {
+            JSONObject person = this.allPerson.getJSONObject(i);
+            JSONArray checkinInfos = person.getJSONArray("checkinInfo");
+            for (int j = 0; j < checkinInfos.size(); j++) {
+                JSONObject checkin = checkinInfos.getJSONObject(i);
+                if (checkin.getJSONObject("bookingNo").getString("bookingNo").equals(no)) {
+                    return person;
+                }
+            }
+        }
+        return null;
+    }
+
+    public JSONObject getBookingByBookingNo(String no) {
+        for (int i = 0; i < this.allPerson.size(); i++) {
+            JSONObject person = this.allPerson.getJSONObject(i);
+            JSONArray checkinInfos = person.getJSONArray("checkinInfo");
+            for (int j = 0; j < checkinInfos.size(); j++) {
+                JSONObject checkin = checkinInfos.getJSONObject(i);
+                if (checkin.getJSONObject("bookingNo").getString("bookingNo").equals(no)) {
+                    return checkin;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean saveData() {
+
+        boolean f = Resources.writeStringData("flight.json", JSON.toJSONString(this.allFlight, true));
+        boolean p = Resources.writeStringData("person.json", JSON.toJSONString(this.allPerson, true));
+        return f && p;
+    }
+
+    public void refreshData() {
+        this.allPerson = JSON.parseArray(Resources.readDataToString("person.json"));
+        this.allFlight = JSON.parseArray(Resources.readDataToString("flight.json"));
+    }
+}
