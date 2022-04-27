11dd4ebcef0efd77c9aa250f6b3344bc7caee0ba
huliang <huliang.2022@bytedance.com>
2022-04-27 22:20:18 +0800

    feat: creditCard

---
+++ src/main/utils/CreditCard.java
@@ -0,0 +1,31 @@
+package main.utils;
+
+public class CreditCard {
+    private String creditCardNo;
+    private String password;
+    /**
+     * @return the creditCardNo
+     */
+    public String getCreditCardNo() {
+        return creditCardNo;
+    }
+    /**
+     * @param creditCardNo the creditCardNo to set
+     */
+    public void setCreditCardNo(String creditCardNo) {
+        this.creditCardNo = creditCardNo;
+    }
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
