aee6fa15de09a7f7308a902552d863d6826da09c
wangjiangshan308 <70676392+wangjiangshan308@users.noreply.github.com>
2022-04-29 22:33:48 +0800

    delete jsonobject

--- src/main/ui/SecondPanel.java
+++ src/main/ui/SecondPanel.java
@@ -262,31 +262,40 @@ public class SecondPanel extends BasePanel implements ActionListener {
 			String surname=jtf5.getText();
 			String id=jtf6.getText();
 
-			JSONObject person=mainFrame.getDataService().getPersonById(id);
+			// JSONObject person=mainFrame.getDataService().getPersonById(id);
 			
-			String name="";
-			try {
-				name=person.getJSONObject("baseInfo").getString("surName");
-			} catch (Exception ex) {
+			// String name="";
+			// try {
+			// 	name=person.getJSONObject("baseInfo").getString("surName");
+			// } catch (Exception ex) {
 			
-			}
+			// }
 
-			if(!name.equals(surname)){
-				JOptionPane.showMessageDialog(null, "ID and name not match!", 
-					"Missing information",JOptionPane.ERROR_MESSAGE);
-			}
+			// if(!name.equals(surname)){
+			// 	JOptionPane.showMessageDialog(null, "ID and name not match!", 
+			// 		"Missing information",JOptionPane.ERROR_MESSAGE);
+			// }
 
-			else{
-				if(jtf.getText().equals("")) bn="None";
-				if(jtf5.getText().equals("")) surname="None";
-				if(jtf6.getText().equals("")) id="None";
-				JSONObject info=JSON.parseObject("{\"booking_num\":\""+bn+"\",\"surname\":\""+surname+"\",\"id\":\""+id+"\"}");
-				String s1 = info.getString("booking_num");
-				String s2 = info.getString("surname");
-				String s3 = info.getString("id");
-				System.out.println(s1+s2+s3);
-				mainFrame.goPanel(Panels.CODE_INPUT, Panels.FLIGHT_INFO,info);
-			}
+			// else{
+			// 	if(jtf.getText().equals("")) bn="None";
+			// 	if(jtf5.getText().equals("")) surname="None";
+			// 	if(jtf6.getText().equals("")) id="None";
+			// 	JSONObject info=JSON.parseObject("{\"booking_num\":\""+bn+"\",\"surname\":\""+surname+"\",\"id\":\""+id+"\"}");
+			// 	String s1 = info.getString("booking_num");
+			// 	String s2 = info.getString("surname");
+			// 	String s3 = info.getString("id");
+			// 	System.out.println(s1+s2+s3);
+			// 	mainFrame.goPanel(Panels.CODE_INPUT, Panels.FLIGHT_INFO,info);
+			// }
+			if(jtf.getText().equals("")) bn="None";
+			if(jtf5.getText().equals("")) surname="None";
+			if(jtf6.getText().equals("")) id="None";
+			JSONObject info=JSON.parseObject("{\"booking_num\":\""+bn+"\",\"surname\":\""+surname+"\",\"id\":\""+id+"\"}");
+			String s1 = info.getString("booking_num");
+			String s2 = info.getString("surname");
+			String s3 = info.getString("id");
+			System.out.println(s1+s2+s3);
+			mainFrame.goPanel(Panels.CODE_INPUT, Panels.FLIGHT_INFO,info);
 			
         
             // mainFrame.goPanel(Panels.CODE_INPUT, Panels.FLIGHT_INFO);
